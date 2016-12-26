64a178395b7958428cd7de3078d44445315ce441
Enrique Garcia <enrique@ravalmatic.com>
2016-12-26 11:33:23 +0100

    grid implemented

--- src/board/Board.ts
+++ src/board/Board.ts
@@ -29,6 +29,22 @@ namespace Xhungo {
                     this.squares[i].push(square);
                 }
             }
+
+            // las lineas
+            let graphics: Phaser.Graphics = new Phaser.Graphics(this.game, 0, 0);
+            this.add(graphics);
+
+            graphics.lineStyle(1, 0xffd900, 1);
+
+            for (let i: number = 0; i < 8; i++) {
+                 // horizontales
+                 graphics.moveTo( -GameConstants.SQUARE_WIDTH / 2,  (i - .5) * GameConstants.SQUARE_WIDTH);
+                 graphics.lineTo( GameConstants.SQUARE_WIDTH * 7.5, (i - .5) * GameConstants.SQUARE_WIDTH);
+
+                 // verticales
+                 graphics.moveTo( (i - .5) * GameConstants.SQUARE_WIDTH, -GameConstants.SQUARE_WIDTH / 2);
+                graphics.lineTo(  (i - .5) * GameConstants.SQUARE_WIDTH, GameConstants.SQUARE_WIDTH * 7.5);
+            }
         }
     }
 }
