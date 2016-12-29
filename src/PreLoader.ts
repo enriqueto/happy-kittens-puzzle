ecd7c8877c53885a0372e7b5be151f2c00e8a645
Enrique Garcia <enrique@ravalmatic.com>
2016-12-29 22:30:21 +0100

    refactoring

--- src/PreLoader.ts
+++ src/PreLoader.ts
@@ -26,24 +26,7 @@ namespace SquaresOut {
 
         private generateBitmapData(): void {
 
-            // generar bitmapdatas
-            let bmd: Phaser.BitmapData =  this.game.add.bitmapData( 16 , 128, "intro_background", true);
-            let grd: CanvasGradient = bmd.ctx.createLinearGradient(0, 0, 0, 128);
-            grd.addColorStop(0, "#11c0e9"); // sets the first color
-            grd.addColorStop(1, "#6fbe4d"); // sets the second color
-
-            bmd.ctx.fillStyle = grd;
-            bmd.ctx.fillRect(0, 0, 16, 128);
-
-            bmd = this.game.add.bitmapData( 16 , 128, "yellow-gradient", true);
-            grd = bmd.ctx.createLinearGradient(0, 0, 0, 128);
-            grd.addColorStop(0, "#FFDC42");
-            grd.addColorStop(1, "#FFB400");
-
-            bmd.ctx.fillStyle = grd;
-            bmd.ctx.fillRect(0, 0, 16, 128);
-
-            bmd =  this.game.add.bitmapData( 64 , 64, GameConstants.RED_SQUARE, true);
+            let bmd: Phaser.BitmapData  =  this.game.add.bitmapData( 64 , 64, GameConstants.RED_SQUARE, true);
             bmd.ctx.beginPath();
             bmd.ctx.rect(0, 0, 64, 64);
             bmd.ctx.fillStyle = "#FF0000";
@@ -72,26 +55,6 @@ namespace SquaresOut {
             bmd.ctx.rect(0, 0, 64, 64);
             bmd.ctx.fillStyle = "#999999";
             bmd.ctx.fill();
-
-            bmd = this.game.add.bitmapData( 16 , 64, "dark_gradient", true);
-            grd = bmd.ctx.createLinearGradient(0, 0, 0, 64);
-            grd.addColorStop(0, "#283939"); // sets the first color
-            grd.addColorStop(1, "#000000"); // sets the second color
-            bmd.ctx.fillStyle = grd;
-            bmd.ctx.fillRect(0, 0, 16, 64);
-
-            bmd = this.game.add.bitmapData( 16 , 16, "preload-bar", true);
-            bmd.ctx.beginPath();
-            bmd.ctx.rect(0, 0, 16, 16);
-            bmd.ctx.fillStyle = "#fc8e00";
-            bmd.ctx.fill();
-
-            bmd = this.game.add.bitmapData(16, 128, "black_gradient", true);
-            for ( let i: number = 0; i < 16; i++) {
-                for ( let j: number = 0; j < 128; j++) {
-                    bmd.setPixel32(i, j, 0, 0, 0, 255 * j / 127);
-                }
-            }
         }
     }
 }
